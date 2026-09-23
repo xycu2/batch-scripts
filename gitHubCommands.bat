3e0741f9d45e367d1cf0b303cec5aa2132b28a4d
xycu <smonindemid@gmail.com>
2026-09-23 22:48:51 +0500

    feat: Add initial setup and repository initialization.

--- gitHubCommands.bat
+++ gitHubCommands.bat
@@ -9,11 +9,13 @@ echo ========================
 echo    GitHub Helpers
 echo ========================
 echo [1] Быстрый push (git add + commit + push)
+echo [2] Иницилизация Репозитория (Запускаем 1 раз)
 echo [0] выход
 
-choice /c 10 /n /m "Выберите действие (1-4, 0): "
+choice /c 120 /n /m "Выберите действие (1-4, 0): "
 
-if errorLevel 2 goto exit
+if errorLevel 3 goto exit
+if errorLevel 2 goto init
 if errorLevel 1 goto quick_push
 
 
@@ -32,8 +34,17 @@ echo [!] Все успешно отправлено на GitHub
 pause
 goto menu
 
+:init
+cls
+echo === Иницилизация Репозитория ===
+git init
+git branch -M main
+set /p REPO_URL="Введите URL репозитория на GitHub: "
+git remote add origin %REPO_URL%
+
+
 :exit
 cls
 echo До свидания!
 timeout /t 2 > nul
-pause
+exit
