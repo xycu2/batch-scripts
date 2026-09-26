c63f05d58187a4a77376f749f48a80aed58c544b
xycu <smonindemid@gmail.com>
2026-09-26 09:40:20 +0500

    feat: add a utility to delete a branch

--- gitHubCommands.bat
+++ gitHubCommands.bat
@@ -12,6 +12,7 @@ echo [1] Быстрый push (git add + commit + push)
 echo [2] Иницилизация Репозитория (Запускаем 1 раз)
 echo [3] Создание ветки (Переходим в нее)
 echo [4] Создание ветки (Остаемся в текущей)
+echo [5] Удаление ветки (git branch -d)
 echo [0] выход
 
 choice /c 12340 /n /m "Выберите действие (1-4, 0): "
@@ -21,6 +22,35 @@ if errorLevel 4 goto create_branch_only
 if errorLevel 3 goto create_and_switch
 if errorLevel 2 goto init
 if errorLevel 1 goto quick_push
+
+:: Удаление ветки
+:delete_branch
+cls
+echo === УДАЛЕНИЕ ВЕТКИ ===
+
+echo.
+echo Вы хотите удалить ветку? (Y/N)
+
+choice /c YN /n /m "Ваш выбор: "
+
+if errorLevel 2 goto menu
+
+set branch_name=
+set /p branch_name="Введите название ветки, которую хотите удалить: "
+
+if "%branch_name%"=="" (
+  echo [!] Название ветки не может быть пустым!
+  pause
+  goto menu
+)
+
+git branch -d "%branch_name%"
+echo Ветка "%branch_name%" успешно удалена!
+
+echo.
+pause
+goto menu
+
 
 :: Создание ветки (остаемся в текущей)
 :create_branch_only
